06208a8bd66d465dd52c508df418601ad85a5617
Rebecca Vogt <rebvogt@gmail.com>
2019-06-07 10:23:28 +0200

    added tests for super_toaster class

--- flying_toaster/super_toaster.py
+++ flying_toaster/super_toaster.py
@@ -18,7 +18,7 @@ class SuperToaster(Toaster):
             super().toast(self._temperature_callback)
 
         else:
-            print(f'You cannot toast any more, because {self.alias} flew away to save the world.')
+            raise Exception(self._message_flying())
     
     def super_boost_toast(self):
         self._temperature = 0
@@ -26,22 +26,28 @@ class SuperToaster(Toaster):
         if not self._flying:
             super().toast(self._boost_callback)
 
+        else:
+            raise Exception(self._message_flying())
+
     def fly(self):
         self._flying = True
 
         print(f'Is it a bird? Is it a plane? No, it is {self.alias}!')
 
     def _temperature_callback(self, second):
-        self._temperature = _linear_increase(second)
+        self._temperature = self._linear_increase(second)
 
         if self._temperature > 500:
             raise Exception('Temperature of toaster exceeded 500 °C. Toasting aborted.')
 
     def _boost_callback(self, second):
-        self._temperature = second ** 2
+        self._temperature = second ** 3
 
         if self._temperature > 1000:
             raise Exception('Splendid, you made the toaster angry. Its skin is turning green and he is going to smash you.')       
     
     def _linear_increase(self, x):
         return 10 * x
+
+    def _message_flying(self):
+        return f'You cannot toast any more, because {self.alias} flew away to save the world.'
---
+++ tests/test_super_toaster.py
@@ -0,0 +1,99 @@
+import os
+import sys
+import pytest
+
+syspath = sys.path.insert(
+    0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from flying_toaster.super_toaster import SuperToaster
+
+test_toaster = SuperToaster(2, 'Super-Toaster')
+
+def test_create_super_toaster():
+    slots = 4
+    alias = 'Black Toaster'
+    colour = 'black'
+
+    super_toaster = SuperToaster(slots, alias, colour)
+
+    assert super_toaster is not None
+    assert isinstance(super_toaster, SuperToaster)
+    assert len(super_toaster.slots) == slots
+    assert super_toaster.alias == alias
+    assert super_toaster.colour == colour
+
+def test_create_super_toaster_wo_colour():
+    slots = 4
+    alias = 'Black Toaster'
+
+    super_toaster = SuperToaster(slots, alias)
+
+    assert super_toaster is not None
+    assert isinstance(super_toaster, SuperToaster)
+    assert len(super_toaster.slots) == slots
+    assert super_toaster.alias == alias
+    assert super_toaster.colour == 'gold'
+
+def test_super_toaster_toast():
+    test_toaster.remove_toasts()
+
+    test_toaster.add_toasts(2, 'Kryptonite')
+    test_toaster.set_timer(3)
+
+    test_toaster.toast()
+
+    for slot in test_toaster.slots:
+        assert slot.toast.roast_level > 0
+
+
+def test_super_toaster_toast_when_flying():
+    flying_toaster = SuperToaster(2, 'Captain Toaster')
+
+    flying_toaster.add_toasts(2, 'Corned Beef')
+    flying_toaster.set_timer(3)
+
+    flying_toaster.fly()
+    
+    with pytest.raises(Exception) as exception:
+        flying_toaster.toast()
+
+def test_super_toaster_toast_overheat():
+    test_toaster.remove_toasts()
+
+    test_toaster.add_toasts(2, 'Bread')
+    test_toaster.set_timer(51)
+
+    with pytest.raises(Exception) as exception:
+        test_toaster.toast()
+
+def test_super_boost_toast():
+    test_toaster.remove_toasts()
+
+    test_toaster.add_toasts(2, 'Bread')
+    test_toaster.set_timer(3)
+
+    test_toaster.super_boost_toast()
+
+    for slot in test_toaster.slots:
+        assert slot.toast.roast_level > 0
+
+def test_super_boost_toast_when_flying():
+    flying_toaster = SuperToaster(2, 'DareToaster', 'red')
+    
+    flying_toaster.add_toasts(2, 'Bread?')
+    flying_toaster.set_timer(3)
+
+    flying_toaster.fly()
+    
+    with pytest.raises(Exception) as exception:
+        flying_toaster.super_boost_toast()
+
+def test_super_boost_toast_make_angry():
+    test_toaster.remove_toasts()
+
+    test_toaster.add_toasts(2, 'Crunchy Stones')
+    test_toaster.set_timer(11)
+
+    with pytest.raises(Exception) as exception:
+        test_toaster.super_boost_toast()
